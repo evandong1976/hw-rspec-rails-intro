ae2d4a5d9da57c5007441e672072ac936f3d3a31
PomatoesBeep <dongerui1976@gmail.com>
2026-04-08 21:45:18 -0400

    added search route

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe MoviesController, type: :controller do
   describe 'searching TMDb' do
-    it 'calls the model method that performs TMDb search'
+    it 'calls the model method that performs TMDb search' do
+      get :search_tmdb, params: { search_terms: 'hardware' }
+    end
     it 'selects the Search Results template for rendering'
     it 'makes the TMDb search results available to that template' 
   end
