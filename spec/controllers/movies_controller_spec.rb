242e95ef8e9c38fc27681ce0a20613757cdd1141
PomatoesBeep <dongerui1976@gmail.com>
2026-04-08 22:01:47 -0400

    Completed

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+"""
 describe MoviesController, type: :controller do
   describe 'searching TMDb' do
     it 'calls the model method that performs TMDb search' do
@@ -8,7 +9,59 @@ describe MoviesController, type: :controller do
         and_return(fake_results)
       get :search_tmdb, params: { search_terms: 'hardware' }
     end
-    it 'selects the Search Results template for rendering'
+    it 'selects the Search Results template for rendering' do
+      fake_results = [double('movie1'), double('movie2')]
+      allow(Movie).to receive(:find_in_tmdb).and_return(fake_results)
+      get :search_tmdb, params: { search_terms: 'hardware' }
+      expect(response).to render_template('search_tmdb')
+    end
     it 'makes the TMDb search results available to that template' 
   end
 end
+"""
+"""
+
+describe MoviesController, type: :controller do
+  describe 'searching TMDb' do
+    before :each do
+      @fake_results = [double('movie1'), double('movie2')]
+    end
+    it 'calls the model method that performs TMDb search' do
+      expect(Movie).to receive(:find_in_tmdb).with('hardware').
+        and_return(@fake_results)
+      get :search_tmdb, params: { search_terms: 'hardware' }
+    end
+    it 'selects the Search Results template for rendering' do
+      allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
+      get :search_tmdb, params: { search_terms: 'hardware' }
+      expect(response).to render_template('search_tmdb')
+    end
+    it 'makes the TMDb search results available to that template'
+  end
+end
+"""
+
+describe MoviesController, type: :controller do
+    describe 'searching TMDb' do
+      before :each do
+        @fake_results = [double('movie1'), double('movie2')]
+      end
+      it 'calls the model method that performs TMDb search' do
+        expect(Movie).to receive(:find_in_tmdb).with('hardware').
+         and_return(@fake_results)
+        get :search_tmdb, params: { search_terms: 'hardware' }
+     end
+     describe 'after valid search' do
+       before :each do
+         allow(Movie).to receive(:find_in_tmdb).and_return(@fake_results)
+         get :search_tmdb, params: { search_terms: 'hardware' }
+       end
+       it 'selects the Search Results template for rendering' do
+         expect(response).to render_template('search_tmdb')
+       end
+       it 'makes the TMDb search results available to that template' do
+         expect(assigns(:movies)).to eq(@fake_results)
+       end
+     end
+   end
+ end
